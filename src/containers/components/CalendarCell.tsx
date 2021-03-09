a771a1a7b69d3285fe8e702390c26587ff01ee47
Evan Tseng <zx086401@gmail.com>
2021-03-09 18:09:25 +0800

    add dialog template

--- src/containers/components/CalendarCell.tsx
+++ src/containers/components/CalendarCell.tsx
@@ -1,4 +1,4 @@
-import { Button, makeStyles } from '@material-ui/core'
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, makeStyles, TextareaAutosize } from '@material-ui/core'
 import React, { useState } from 'react'
 
 const useStyle = makeStyles({
@@ -28,10 +28,19 @@ export default function CalendarCell(props: Props) {
   const classes = useStyle()
   const [showDialog, toggleDialog] = useState(false)
 
-  return (
+  return <>
     <div className={classes.cell} onClick={() => toggleDialog(true)}>
-      <div className={"title"}>{day}</div>
+      <div className={'title'}>{day}</div>
       <div>something to do</div>
     </div>
-  )
+    <Dialog open={showDialog}>
+      <DialogTitle>{day}'s schedule</DialogTitle>
+      <DialogContent>
+        <TextareaAutosize style={{width: 280, height: 100}}></TextareaAutosize>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={() => toggleDialog(false)}>confirm</Button>
+      </DialogActions>
+    </Dialog>
+  </>
 }
