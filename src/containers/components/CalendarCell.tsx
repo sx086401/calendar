4e598e0b4eec468b1205aba0f60d5499a27c60d4
Evan Tseng <zx086401@gmail.com>
2021-03-10 19:04:13 +0800

    fix dialog style and the note

--- src/containers/components/CalendarCell.tsx
+++ src/containers/components/CalendarCell.tsx
@@ -1,4 +1,4 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, makeStyles, TextareaAutosize } from '@material-ui/core'
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, makeStyles, Typography } from '@material-ui/core'
 import React, { useState } from 'react'
 
 const useStyle = makeStyles({
@@ -9,13 +9,45 @@ const useStyle = makeStyles({
     height: '100px',
     width: '100%',
     '&:hover': {
-      backgroundColor: 'rgb(244, 244, 244)'
+      backgroundColor: 'rgb(139, 144, 152, 0.5)'
     },
     '& .title': {
       textAlign: 'center',
-      marginTop: 5,
+      margin: 5,
       width: '100%',
+      height: 10
+    },
+    '& .note': {
+      width: '100%',
+      lineHeight: '1.4em',
+      maxHeight: '2.8em',
+      display: '-webkit-box',
+      WebkitLineClamp: 2,
+      WebkitBoxOrient: 'vertical',
+      overflow: 'hidden',
+      textOverflow: 'ellipsis',
+      padding: 1,
     }
+  },
+  dialog: {
+    backgroundColor: 'rgb(244, 244, 244, 0.8)',
+    overflow: 'hidden',
+    '& .content': {
+      backgroundColor: 'rgb(39, 44, 52, 0.7)',
+      color: 'white'
+    }
+  },
+  textArea: {
+    resize: 'none',
+    outline: 'none',
+    backgroundColor: 'rgb(39, 44, 52, 0.1)',
+    color: 'white',
+    fontSize: 18,
+    width: 380,
+    height: 300,
+  },
+  button: {
+    color: 'white'
   }
 })
 
@@ -28,18 +60,23 @@ export default function CalendarCell(props: Props) {
   const classes = useStyle()
   const [showDialog, toggleDialog] = useState(false)
 
+  const onClose = () => {
+    toggleDialog(false)
+  }
+
   return <>
     <div className={classes.cell} onClick={() => toggleDialog(true)}>
       <div className={'title'}>{day}</div>
-      <div>something to do</div>
+      <Typography variant="body1" align='center' className={'note'}>something to do on day {day}</Typography>
     </div>
-    <Dialog open={showDialog}>
-      <DialogTitle>{day}'s schedule</DialogTitle>
-      <DialogContent>
-        <TextareaAutosize style={{width: 280, height: 100}}></TextareaAutosize>
+    <Dialog className={classes.dialog} open={showDialog}>
+      <DialogTitle className='content'>{day}'s schedule</DialogTitle>
+      <DialogContent className='content' dividers>
+        <textarea className={classes.textArea}></textarea>
       </DialogContent>
-      <DialogActions>
-        <Button onClick={() => toggleDialog(false)}>confirm</Button>
+      <DialogActions className='content'>
+        <Button className={classes.button} onClick={onClose}>cancel</Button>
+        <Button className={classes.button} onClick={onClose}>confirm</Button>
       </DialogActions>
     </Dialog>
   </>
