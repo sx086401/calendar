c3124f92e0ef8f964b96f523b37c7a9e5d4144d4
Evan Tseng <zx086401@gmail.com>
2021-03-18 15:35:24 +0800

    add react hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { HashRouter, Switch, Route } from 'react-router-dom'
 import './App.css';
-import Calendar from './containers/components/Calendar';
+import Calendar from './containers/calendar/Calendar';
+import Index from './containers/index';
 
 const App: React.FC = () => {
   return (
-    <Calendar></Calendar>
+    <HashRouter>
+      <Switch>
+        <Route exact path='/calendar'>
+          <Calendar></Calendar>
+        </Route>
+        <Route path='/'>
+          <Index></Index>
+        </Route>
+      </Switch>
+    </HashRouter>
   )
 }
 
